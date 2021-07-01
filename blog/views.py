44691b39de6e1d419a9b821b108a76ae732ed93c
Emmanuel Debrah <kweku.debrah.emma@gmail.com>
2021-07-01 23:07:06 +0000

    templating and static css

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Kweku Debrah',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2019'
+    },
+    {
+        'author': 'Bismark Debrah',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 30, 2019'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, template_name='blog/home.html', context=context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, template_name='blog/about.html')
