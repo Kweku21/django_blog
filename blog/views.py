e0a67c30ea92db2ec8362b8b8b6afa83315d8bb8
Emmanuel Debrah <kweku.debrah.emma@gmail.com>
2021-07-02 00:12:40 +0000

    database and migration

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,17 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Kweku Debrah',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2019'
-    },
-    {
-        'author': 'Bismark Debrah',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 30, 2019'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, template_name='blog/home.html', context=context)
 
 
 def about(request):
-    return render(request, template_name='blog/about.html')
+    context = {
+        'title': 'About'
+    }
+    return render(request, template_name='blog/about.html', context=context)
